e24540ae791dddd25cf3cf2406c7222d2d08bfca
Christopher Chaison <cchaison0@gmail.com>
2020-03-19 22:17:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
   # location: project.css("ul.project-meta span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
-  projecs = {}
+  projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
